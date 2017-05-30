6e3be661547160e7c3cf435e44bab26f42f0fb96
Karl Broman <kbroman@gmail.com>
2017-05-30 10:31:04 -0500

    Fill out package; initial functions elsafy() and de_elsafy()

---
+++ R/elsafy.R
@@ -0,0 +1,77 @@
+#' Give a player Elsa powers
+#'
+#' Give a player Elsa powers: when she walks on water, it turns to ice.
+#'
+#' @param player_id Player's entity ID
+#' @param water Vector of item IDs for different kinds of water
+#' @param ice Item ID for ice
+#' @param delay Delay (in seconds) between calls to the minecraft server
+#'
+#' @return None.
+#'
+#' @examples
+#' \dontrun{
+#' # Runs in an infinite loop; press ctrl-C to stop.
+#' elsafy(215)
+#' }
+#'
+#' @seealso \code{\link{de_elsafy}}
+#' @export
+elsafy <- function(player_id=NULL, water=c(8, 9), ice=174, delay=0.02)
+{
+    while(TRUE) {
+        pos <- miner::getPlayerPos(player_id)
+        block <- miner::getBlocks(pos[1]-1, pos[2]-1, pos[3]-1,
+                                  pos[1]+1, pos[2]-1, pos[3]+1)
+        for(i in -1:1) {
+            for(j in -1:1) {
+                if(block[i+2,1,j+2] %in% water)
+                    miner::setBlock(pos[1]+i, pos[2]-1, pos[3]+j, ice)
+            }
+        }
+
+        Sys.sleep(delay)
+    }
+}
+
+#' Give a player reverse Elsa powers
+#'
+#' Give a player revsese Elsa powers, to clean up after running \code{\link{elsafy}}: when she walks over ice, it turns back to water.
+#'
+#' @param player_id Player's entity ID
+#' @param water Item IDs for water
+#' @param ice Item ID for ice
+#' @param delay Delay (in seconds) between calls to the minecraft server
+#'
+#' @return None.
+#'
+#' @examples
+#' \dontrun{
+#' # Runs in an infinite loop; press ctrl-C to stop.
+#' de_elsafy(215)
+#' }
+#'
+#' @seealso \code{\link{elsafy}}
+#' @export
+de_elsafy <- function(player_id=NULL, water=8, ice=174, delay=0.05)
+{
+    # range of blocks around Elsa to be cleared
+    d <- list(-1:1, -5:0, -1:1)
+
+    while(TRUE) {
+        pos <- miner::getPlayerPos(player_id)
+        block <- miner::getBlocks(pos[1]+min(d[[1]]), min(pos[2]+d[[2]]), min(pos[3]+d[[3]]),
+                                  pos[1]+max(d[[1]]), max(pos[2]+d[[2]]), max(pos[3]+d[[3]]))
+        for(i in seq_along(d[[1]])) {
+            for(j in seq_along(d[[2]])) {
+                for(k in seq_along(d[[3]])) {
+                    if(block[i,j,k] == ice)
+                        miner::setBlock(pos[1]+d[[1]][i],
+                                        pos[2]+d[[2]][j],
+                                        pos[3]+d[[3]][k],
+                                        water)
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+## miner.extra
+
+[![Travis-CI Build Status](https://travis-ci.org/ROpenSciLabs/miner.extra.svg?branch=master)](https://travis-ci.org/ROpenSciLabs/miner.extra>)
+
+The [miner R package](https://github.com/ROpenSciLabs/miner) makes it
+possible to interact with [Minecraft](https://minecraft.net) from
+[R](https://www.r-project.org).
+
+This package,
+[miner.extra](https://github.com/ROpenSciLabs/miner.extra), includes
+additional functions that illustrate and extend the use of miner.
+
+
+### About this package
+
+This package was created as part of the [ROpenSci unconference](http://unconf17.ropensci.org/)
+in May, 2017 by:
+
+* [Brooke Anderson](https://github.com/geanders)
+* [Karl Broman](https://github.com/kbroman)
+* [Gergely Daróczi](https://github.com/daroczig)
+* [Mario Inchiosa](https://github.com/inchiosa)
+* [David Smith](https://github.com/revodavid)
+* [Ali Zaidi](https://github.com/akzaidi)
+
+### License
+
+Licensed under the [MIT license](https://cran.r-project.org/web/licenses/MIT). ([More information here](https://en.wikipedia.org/wiki/MIT_License).)
