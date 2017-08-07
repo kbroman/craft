b086b951ff6ebc494f7be2981f48eaa2e97cae0f
Your Name <nwstephens@gmail.com>
2017-08-06 21:34:14 -0400

    adding sphere and cube functions

---
+++ R/cube.R
@@ -0,0 +1,73 @@
+#' Make a cube
+#'
+#' Make a solid or hollow cube.
+#'
+#' @param xlen integer. Cube length.
+#' @param ylen integer. Cube hight.
+#' @param zlen integer. Cube depth.
+#' @param blockid integer. Block type.
+#' @param styleid integer. Block style.
+#' @param fill logical. If TRUE then solid. If FALSE then hollow.
+#' @param offset vector of length three. How many units to offset the cube from \code{pos}.
+#' @param playerid integer of length one. Defaults to player id in solo play. Set explicitly in multi play.
+#' @param pos vector of length three. Defines the corner of the cube. Defaults to the player's position.
+#' @param xlim vector of length two. Defines the lower and upper cuttoff points to truncate the cube.
+#' @param ylim vector of length two. Defines the lower and upper cuttoff points to truncate the cube.
+#' @param zlim vector of length two. Defines the lower and upper cuttoff points to truncate the cube.
+#'
+#' @return Builds a cube or cuboid with the corner at the player's position. By default, the cube is hollow, but you can also use \code{fill=TRUE} to create a solid cube. The players position is determined by \code{getPlayerPos}. You can you can reposition the cube with the \code{offset} command. Use \code{xlim}, \code{ylim}, \code{zlim} to truncate the cube to create a fence or wall. This function will return the cube origin.
+#' @export
+#'
+#' @seealso \code{\link{sphere}} to create a sphere.
+#'
+#' @examples
+#' # Stone cube 10x10x10
+#' cube(pos = c(0, 0, 0))
+#'
+#' # Erase cube
+#' cube(blockid = 0, pos = c(0, 0, 0))
+#'
+#' # Stone fence
+#' cube(ylim = c(2, 3), offset = c(0, -2, 0))
+#'
+#'
+cube <- function(
+  xlen = 10, ylen = 10, zlen = 10,
+  blockid = 1,
+  styleid = 0,
+  fill = FALSE,
+  offset = c(0, -1, 0),
+  playerid = getPlayerIds(),
+  pos, xlim, ylim, zlim
+){
+
+    if(missing(pos)){
+        if(length(playerid) != 1) stop("playerid must be of length one")
+        pos <- getPlayerPos(playerid, tile = TRUE)
+    }
+
+    s <- 1 - fill
+    xvec <- c(0, rep(s, xlen - 2), 0)
+    yvec <- c(0, rep(s, ylen - 2), 0)
+    zvec <- c(0, rep(s, zlen - 2), 0)
+    prism <- 1 - outer(outer(xvec, yvec), zvec)
+
+    if(missing(xlim)) xlim <- c(1, xlen)
+    if(missing(ylim)) ylim <- c(1, ylen)
+    if(missing(zlim)) zlim <- c(1, zlen)
+    xx <- seq(xlim[1], xlim[2])
+    yy <- seq(ylim[1], ylim[2])
+    zz <- seq(zlim[1], zlim[2])
+
+    p <- pos + offset
+    for(x in xx){
+    for(y in yy){
+      for(z in zz){
+        if(prism[x, y, z] == 1){
+          setBlock(x + p[1], y + p[2], z + p[3], blockid, styleid)
+        }
+      }
+    }
+    }
+    pos
+}
---
+++ R/sphere.R
@@ -0,0 +1,100 @@
+#' Make a sphere
+#'
+#' Make a solid or hollow sphere with the player at the center.
+#'
+#' @param radius integer. Specifies the radius of the sphere.
+#' @param blockid integer. Block type to be used.
+#' @param styleid integer. Block style to be used.
+#' @param fill logical. Should the sphere be solid or hollow. Defaults to hollow.
+#' @param offset vector of length three. Defines where to place the sphere with respect to \code{pos}.
+#' @param playerid integer of length one. Defaults to player id in solo play. Set explicitly in multi play.
+#' @param pos vector of length three. Defines the center of the sphere. Defaults to the player id position.
+#' @param xlim vector of length two. Defines the lower and upper cuttoff points to truncate the sphere.
+#' @param ylim vector of length two. Defines the lower and upper cuttoff points to truncate the sphere.
+#' @param zlim vector of length two. Defines the lower and upper cuttoff points to truncate the sphere.
+#'
+#' @return Builds a sphere (or a truncated sphere) around the players current position. By default, the sphere is hollow, but you can also use \code{fill=TRUE} to create a solid sphere. The players position is determined by \code{getPlayerPos}. You can you can reposition the sphere with the \code{offset} command. Use \code{xlim}, \code{ylim}, \code{zlim} to truncate the sphere. This function will return the sphere's origin.
+#' @export
+#'
+#' @seealso \code{\link{cube}} to create a cube.
+#'
+#' @examples
+#' # Hollow glass sphere of size 15
+#' sphere(pos = c(0, 0, 0))
+#'
+#' # Erase sphere
+#' sphere(blockid = 0, pos = c(0, 0, 0))
+#'
+#' # Glass dome
+#' sphere(ylim = c(0, 15))
+#'
+sphere <- function(
+    radius = 15,
+    blockid = 20,
+    styleid = 0,
+    fill = FALSE,
+    offset = c(0, 0, 0),
+    playerid = getPlayerIds(),
+    pos, xlim, ylim, zlim
+){
+
+    if(missing(pos)){
+        if(length(playerid) != 1) stop("playerid must be of length one")
+        pos <- getPlayerPos(playerid, tile = TRUE)
+    }
+
+    # Create a stack of rings
+    rings <- array(0, c(x = 2 * radius + 1, y = radius + 1, z = 2 * radius + 1))
+    for(y in 0:radius){
+      for(theta in seq(0, 2 * pi, len = 10 * radius)){
+        xind <- sqrt(radius ^ 2 - y ^ 2) * cos(theta) + radius
+        zind <- sqrt(radius ^ 2 - y ^ 2) * sin(theta) + radius
+        rings[round(xind) + 1, y + 1, round(zind) + 1] <- 1
+      }
+    }
+
+    # Create a solid dome
+    fillrow <- function(x){
+      ind <- which(x == 1)
+      if(length(ind) == 0) ind <- 0
+      rng <- range(ind)
+      idx <- seq_along(x)
+      fill <- idx >= min(rng) & idx <= max(rng)
+      fill * 1
+    }
+    dome <- aperm(apply(rings, 1:2, fillrow), c(2, 3, 1))
+
+    # Hollow out the dome
+    if(!fill){
+      for(y in 1:radius){
+        disk <- (dome[, y, ] - dome[, y + 1, ]) == 1
+        ring <- rings[, y, ] == 1
+        dome[, y, ] <- (disk | ring) * 1
+      }
+    }
+
+    # Create a sphere
+    yind <- abs(seq(-radius, radius)) + 1
+    sphere <- dome[, yind, ,drop = FALSE]
+
+    # Define plot dimensions
+    if(missing(xlim)) xlim <- c(-radius, radius)
+    if(missing(ylim)) ylim <- c(-radius, radius)
+    if(missing(zlim)) zlim <- c(-radius, radius)
+    xx <- seq(xlim[1], xlim[2]) + radius + 1
+    yy <- seq(ylim[1], ylim[2]) + radius + 1
+    zz <- seq(zlim[1], zlim[2]) + radius + 1
+
+    # Place blocks to create the sphere
+    p <- pos + offset - c(radius + 1, radius + 1, radius + 1)
+    for(x in xx){
+      for(y in yy){
+        for(z in zz){
+          if(sphere[x, y, z] == 1){
+            setBlock(x + p[1], y + p[2], z + p[3], blockid, styleid)
+          }
+        }
+      }
+    }
+    pos
+}
