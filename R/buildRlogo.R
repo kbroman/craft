963e1c55d08bc7609d1a48b8275e419eac172893
Karl Broman <kbroman@gmail.com>
2026-05-12 16:51:10 -0500

    buildRlogo: add dir argument

--- R/buildRlogo.R
+++ R/buildRlogo.R
@@ -9,6 +9,8 @@
 #' @param width Width of R logo in blocks
 #' (if not provided, determined to preserve aspect ratio
 #'
+#' @param dir Which direction should the logo go?
+#'
 #' @importFrom imager resize
 #' @importFrom miner setBlock
 #'
@@ -23,8 +25,11 @@
 #' }
 
 buildRlogo <-
-    function(lowerleft, height=80, width=NULL)
+    function(bottomleft, height=80, width=NULL,
+             dir=c("north", "south", "east", "west"))
 {
+    dir <- match.arg(dir)
+
     # download file
     data(Rlogo)
 
@@ -37,19 +42,34 @@ buildRlogo <-
     blue <- data.frame(name="Blue Wool", id=35, style=11) # miner::find_item("Blue Wool")
     gray <- data.frame(name="Light Gray Wool", id=35, style=8) # miner::find_item("Light Gray Wool")
 
+    # east =  positive x   west =  negative x
+    # south = positive z   north = negative z
+    x <- x0 <- bottomleft[1]
+    y <- y0 <- bottomleft[2]
+    z <- z0 <- bottomleft[3]
+
     for(i in 1:nrow(Rlogo)) {
         for(j in 1:ncol(Rlogo)) {
-            if(Rlogo[i,j] == 2)
-                miner::setBlock(lowerleft[1]+(nrow(Rlogo)-i),
-                                lowerleft[2]+(ncol(Rlogo)-j),
-                                lowerleft[3],
+            if(Rlogo[i,j] == 2 || Rlogo[i,j] == 3) {
+                if(dir == "north") {
+                    z <- z0 - (i-1)
+                } else if(dir == "south") {
+                    z <- z0 + (i-1)
+                } else if(dir == "east") {
+                    x <- x0 + (i-1)
+                } else {
+                    x <- x0 - (i-1)
+                }
+
+                y <- y0 + nrow(Rlogo) - (j-1)
+            }
+
+            if(Rlogo[i,j]==2)
+                miner::setBlock(x, y, z,
                                 blue[2], blue[3])
             if(Rlogo[i,j] == 3)
-                miner::setBlock(lowerleft[1]+(nrow(Rlogo)-i),
-                                lowerleft[2]+(ncol(Rlogo)-j),
-                                lowerleft[3],
+                miner::setBlock(x, y, z,
                                 gray[2], gray[3])
         }
     }
-
 }
